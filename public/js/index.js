ccc83a15c876d2df4c44d5b13dcd7b95d568131b
m0wh <malo.dewiderspach@gmail.com>
2018-04-12 03:00:58 +0200

    :lipstick: update UI

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,5 @@
 var socket = io();
+var myUsername = 'Anonymous' + Math.round(Math.random()*9999);
 
 socket.on('connect', function() {
   console.log('Connected to server');
@@ -9,27 +10,47 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
-  console.log('New message', message);
-  var li = $('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+  var formattedTime = moment(message.createdAt).format('hh:mm');
+  var template = $('#message-template').html();
+  if(message.from == 'Admin') {
+    template = $('#message-admin-template').html();
+  } else if (message.from == myUsername) {
+    template = $('#message-sent-template').html();
+  }
+  var html = Mustache.render(template, {
+    from: message.from,
+    text: message.text,
+    createdAt: formattedTime
+  });
 
-  $('#messages').append(li);
+  $('#messages').append(html);
+
+  $("#messages").scrollTop($("#messages")[0].scrollHeight);
 });
 
-// socket.emit('createMessage', {
-//   from: 'Frank',
-//   text: 'Hi'
-// }, function(data) {
-//   console.log('Got it.', data);
-// });
 
 $('#message-form').on('submit', function(e) {
   e.preventDefault();
+
+  var messageInput = $('[name=message]').val();
+
   socket.emit('createMessage', {
-    from: 'Anonymous',
-    text: $('[name=message]').val()
+    from: myUsername,
+    text: messageInput
   }, function(data) {
-    console.log('Got it.', data);
+    $('[name=message]').val("");
   });
-  $('[name=message]').val("");
 });
+
+resizeChat();
+$(window).resize(function() {
+  resizeChat();
+});
+
+function resizeChat() {
+  $('#messages').height(window.innerHeight-150);
+}
+
+
+// moment.locale('fr');
+// moment(1316116057189).fromNow();
